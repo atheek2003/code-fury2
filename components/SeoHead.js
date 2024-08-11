4dda2191612ce824f442e09b3e2845f81db42412
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-08-11 07:26:33 +0530

    backend

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -9,7 +9,7 @@ const defaultMeta = {
   description:
     'Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest',
   // change base url of your web (without '/' at the end)
-  url: 'https://next-landing-vpn.vercel.app',
+  url: 'https://code-fury-steel.vercel.app',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
